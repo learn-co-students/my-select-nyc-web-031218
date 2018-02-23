a5bd36a06fe8b7eff00caa9d46657cae6478a84c
p-wong <patriciawong08@gmail.com>
2018-02-23 17:54:02 -0500

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
-def my_select(collection)
+def my_select(array)
  # your code here!
+ i = 0
+ select_array = []
+ while i < array.length
+   if yield(array[i])
+     select_array << array[i]
+   end
+   i += 1
+ end
+ select_array
 end
