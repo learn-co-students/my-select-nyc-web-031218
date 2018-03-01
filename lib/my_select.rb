c4ef411ca0a08e11f61dd47fd49e04f47ef4fe59
Arthur Lurye <arthurlurye@gmail.com>
2018-03-01 09:23:50 -0500

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
+  i = 0
  # your code here!
+ newBlock = []
+ while i < collection.length
+
+   if collection[i] % 2 == 0
+     newBlock.push(collection[i])
+   end
+  i+=1
+ end
+ newBlock
 end
